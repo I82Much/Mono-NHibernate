57612708f5f4825de857cf85eaaff567adc6c311
Nick Dunn <ndunn@mitre.org>
2011-05-02 15:48:33 -0400

    Initial commit with SQLITE working on Mono.

---
+++ NHibernateTest/Domain/Product.cs
@@ -0,0 +1,12 @@
+using System;
+namespace NHibernateTest.Domain
+{
+	public class Product
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Category { get; set; }
+		public virtual bool Discontinued { get; set; }
+	}
+}
+
---
+++ NHibernateTest/Main.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernateTest.Domain;
+using NHibernate.Tool.hbm2ddl;
+
+namespace NHibernateTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			IList<Product> products; 
+			
+			// Don't need to use schema export because of the hbm2dll property.
+			var cfg = new NHibernate.Cfg.Configuration();
+		    cfg.Configure();
+			// ensure that mapping hbm.xml file is loaded
+			cfg.AddAssembly(typeof(MainClass).Assembly);
+			
+			
+			
+			Product p = new Product() {Name="Captains of Crush Gripper #1", Category="fitness" };
+			
+			ISessionFactory factory = 
+				cfg.BuildSessionFactory();
+			
+			
+			
+			using (ISession session = factory.OpenSession()) 
+			{ 
+				session.Save(p);
+				session.Flush();
+				
+			  	ICriteria sc = session.CreateCriteria<Product>(); 
+			  	products = sc.List<Product>();
+				Console.WriteLine(products[0].Name);
+			  	session.Close(); 
+			} 
+			factory.Close(); 
+			
+			Console.WriteLine( products.Count );
+			
+			
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
+
